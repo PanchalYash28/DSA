f48d23ead3cb31ff02e4b07c845fd42012d8c2af
PanchalYash28 <yashpanchal0091@gmail.com>
2025-01-03 14:51:03 +0530

    Update global.py

--- Functions/global.py
+++ Functions/global.py
@@ -7,7 +7,7 @@ def add():
     c = c + 2
     print(c)
 add() 
-# UnboundLocalError: local variable 'c' referenced before assignment
+# Output: UnboundLocalError: local variable 'c' referenced before assignment
 
 """ Example: Changing Global Variable From Inside a Function using global"""
 
@@ -20,15 +20,12 @@ def add():
     c = c + 2 
     print(c)
 add()
-
-
+# Output: 3
 
 
 """ What is __name__ in Python? """
 
 # The __name__ variable is a special builtin Python variable that shows the name of the current module.
-
-
 
 
 """ Python Modules"""
@@ -40,14 +37,13 @@ import math
 
 # use math.pi to get value of pi
 print("The value of pi is", math.pi)
-# The value of pi is 3.141592653589793
+# Output: The value of pi is 3.141592653589793
 
 """ Python import with Renaming """
 # import module by renaming it
 import math as m
 
 print(m.pi)
-
 # Output: 3.141592653589793
 
 """" Python from...import statement"""
@@ -55,7 +51,6 @@ print(m.pi)
 from math import pi
 
 print(pi)
-
 # Output: 3.141592653589793
 
 """ Import all names """
@@ -63,16 +58,15 @@ print(pi)
 from math import *
 
 print("The value of pi is", pi)
+# Output: The value of pi is 3.141592653589793
 
 """ The dir() built-in function"""
-print(dir(example))
-
-['__builtins__',
-'__cached__',
-'__doc__',
-'__file__',
-'__initializing__',
-'__loader__',
-'__name__',
-'__package__',
-'add']
+print(dir())
+# Output:
+['__annotations__', '__builtins__', '__doc__', '__loader__', '__name__', '__package__',
+ '__spec__', 'acos', 'acosh', 'asin', 'asinh', 'atan', 'atan2', 'atanh', 'cbrt', 'ceil',
+ 'comb', 'copysign', 'cos', 'cosh', 'degrees', 'dist', 'e', 'erf', 'erfc', 'exp', 'exp2',
+ 'expm1', 'fabs', 'factorial', 'floor', 'fmod', 'frexp', 'fsum', 'gamma', 'gcd', 'hypot',
+ 'inf', 'isclose', 'isfinite', 'isinf', 'isnan', 'isqrt', 'lcm', 'ldexp', 'lgamma', 'log',
+ 'log10', 'log1p', 'log2', 'modf', 'nan', 'nextafter', 'perm', 'pi', 'pow', 'prod', 'radians',
+ 'remainder', 'sin', 'sinh', 'sqrt', 'sumprod', 'tan', 'tanh', 'tau', 'traceback', 'trunc', 'ulp']
